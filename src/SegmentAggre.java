195f23714f243a47e9bc118b96050ce66a5ad183
umbrellaAcademy <mathieucormier07@gmail.com>
2025-04-04 15:57:28 -0400

    projet done part 1

---
+++ src/SegmentAggre.java
@@ -0,0 +1,15 @@
+public class SegmentAggre {
+    private Point p1;
+    private Point p2;
+
+    public SegmentAggre(Point p1, Point p2){
+        this.p1 = p1;
+        this.p2 = p2;
+
+        }
+
+    @Override
+    public String toString(){
+        
+    }
+}
---
+++ src/SegmentCompo.java
@@ -0,0 +1,10 @@
+public class SegmentCompo {
+    private Point p1;
+    private Point p2;
+
+    public SegmentCompo(int x1, int y1, int x2, int y2){
+        this.p1 = new Point(x1, y1);
+        this.p2 = new Point(x2, y2);
+    }
+
+}
